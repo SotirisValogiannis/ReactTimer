313c4323ac8dffbc3123f1645cf5c799d79daa66
Sotiris Valogiannis <sotirisvalogiannis@macbooks-MacBook-Pro.local>
2016-08-13 23:07:03 +0300

    Added clock tests and clock

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,12 @@
 var React = require('react');
+var Clock = require('Clock')
 
 var Countdown = React.createClass({
   render: function(){
     return(
-      <div>Countdown.jsx</div>
+      <div>
+        <Clock totalSeconds={129}></Clock>
+      </div>
     );
   }
 })
